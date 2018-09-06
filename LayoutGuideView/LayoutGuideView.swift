40b40cdc6f241c069112f0eff6179d9cca5cc5a5
Gabriele Trabucco <gabrynet83@gmail.com>
2018-09-06 23:24:23 +0200

    LayoutGuideView initial implementation

---
+++ LayoutGuideView/LayoutGuideView.swift
@@ -0,0 +1,83 @@
+//
+//  LayoutGuideView.swift
+//  LayoutGuideView
+//
+//  Created by Gabriele Trabucco on 06/09/2018.
+//  Copyright © 2018 Gabriele Trabucco. All rights reserved.
+//
+
+import Foundation
+
+public class LayoutGuideView: UIView {
+
+    private let guide: UILayoutGuide = {
+        let guide = UILayoutGuide()
+        guide.identifier = "Generated-by-LayoutGuideView"
+        return guide
+    }()
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        fatalError("Programmatically initialization is not supported. This class is only suitable for initialization by nib or storyboard")
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    public override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+        guard newSuperview?.layoutGuides.contains(guide) == false else { return }
+        newSuperview?.addLayoutGuide(guide)
+    }
+
+    public override func updateConstraints() {
+
+        if let superview = superview {
+            replaceConstraints(constraints)
+            replaceConstraints(superview.constraints)
+            removeFromSuperview()
+        }
+
+        super.updateConstraints()
+    }
+
+    private func replaceConstraints(_ constraints: [NSLayoutConstraint]) {
+
+        let replacements: (active: [NSLayoutConstraint], inactive: [NSLayoutConstraint]) = constraints.reduce((active: [], inactive: [])) {
+            guard var item = $1.firstItem else { return $0 }
+            let attribute = $1.firstAttribute
+            let relatedBy = $1.relation
+            var toItem = $1.secondItem
+            let secondAttribute = $1.secondAttribute
+            let multiplier = $1.multiplier
+            let constant = $1.constant
+
+            switch (item, toItem) {
+            case let (i as LayoutGuideView, t as LayoutGuideView):
+                item = i.guide
+                toItem = t.guide
+            case let (i as LayoutGuideView, _) where i === self:
+                item = i.guide
+            case let (_, t as LayoutGuideView) where t === self:
+                toItem = t.guide
+            default:
+                return $0
+            }
+
+            let replacement = NSLayoutConstraint(item: item, attribute: attribute,
+                                                 relatedBy: relatedBy,
+                                                 toItem: toItem, attribute: secondAttribute,
+                                                 multiplier: multiplier, constant: constant)
+
+            if $1.isActive {
+                return ($0.active + [replacement], $0.inactive)
+            } else {
+                return ($0.active, $0.inactive + [replacement])
+            }
+        }
+
+        NSLayoutConstraint.activate(replacements.active)
+        NSLayoutConstraint.deactivate(replacements.inactive)
+    }
+}
---
+++ LayoutGuideViewExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LayoutGuideViewExample
+//
+//  Created by Gabriele Trabucco on 06/09/2018.
+//  Copyright © 2018 Gabriele Trabucco. All rights reserved.
+//
+
+import UIKit
+import LayoutGuideView
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
