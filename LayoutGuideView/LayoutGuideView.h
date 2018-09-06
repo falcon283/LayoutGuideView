695fcd2df7d8c08624a601b694b7cfac7047f1a3
Gabriele Trabucco <g.trabucco@reply.it>
2018-09-06 20:32:34 +0200

    Add Project

---
+++ LayoutGuideView/LayoutGuideView.h
@@ -0,0 +1,19 @@
+//
+//  LayoutGuideView.h
+//  LayoutGuideView
+//
+//  Created by Gabriele Trabucco on 06/09/2018.
+//  Copyright © 2018 Gabriele Trabucco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LayoutGuideView.
+FOUNDATION_EXPORT double LayoutGuideViewVersionNumber;
+
+//! Project version string for LayoutGuideView.
+FOUNDATION_EXPORT const unsigned char LayoutGuideViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LayoutGuideView/PublicHeader.h>
+
+
